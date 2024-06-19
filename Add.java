12699ce47741069d9daaea46c985e97690ff331f
Sasikala Chirumamilla <sasichirumamilla22@gmail.com>
2024-06-19 12:30:50 +0530

    Basic Syntax of Java Programming

---
+++ Add.java
@@ -0,0 +1,11 @@
+class Add{
+public static void main(String[] args)
+{
+System.out.println("Addition of 2 numbers");
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println("Final addition is:"+c);
+}
+
+}
